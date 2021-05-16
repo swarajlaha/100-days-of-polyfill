6552b26c2660f39d135e529cfd2e4e4286c05bbc
swarajlaha <swaraj.srj@gmail.com>
2021-05-16 12:56:21 +0530

    day 1

--- Day 1/filter.js
+++ Day 1/filter.js
@@ -2,7 +2,7 @@
  * Polyfill - Array.prototype.filter()
  * The filter() method creates a new array with all elements that pass the test implemented by the provided function.
  * The below user defined function is a polyfill for the .filter() function, which is predefined in the prototype of Array object.
- * myFilter() - Takes a callback as it's argument and returns a new array of element(s), whose each element would be travered and the callback function implemented on it. IF the callback function condition is met, the corresponding element is added to the new array.
+ * myFilter() - Takes a callback as it's argument and returns a new array of element(s), whose each element would be traversed and the callback function implemented on it. IF the callback function condition is met, the corresponding element is added to the new array.
  */
 
 const arr = [1, 2, 3];
--- Day 1/map.js
+++ Day 1/map.js
@@ -2,22 +2,20 @@
  * Polyfill - Array.prototype.map()
  * The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
  * The below user defined function is a polyfill for the .map() function, which is predefined in the prototype of Array object.
- * myMap() - Takes a callback as it's argument and returns a new array, whose each element would be travered and the callback function implemented on it.
+ * myMap() - Takes a callback as it's argument and returns a new array, whose each element would be traversed and the callback function implemented on it.
  */
 
 const arr = [1, 2, 3];
 
 Array.prototype.myMap = function(callback) {
-
   let resArr = [];
 
   for(let i=0; i<this.length; i++) {
     const eachEle = callback(this[i]);
     resArr.push(eachEle);
   }
-
   return resArr;
 };
 
-arr.myMap(ele => ele);
-// arr.myMap(ele => ele + 1);
+arr.myMap(ele => ele);  // O/p: [1, 2, 3]
+// arr.myMap(ele => ele + 1); // O/p: [2, 3, 4]
